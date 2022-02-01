0e1922a31adb465a9940af6041f32ee937fe8a16
Carlos Eduardo Garcia Alcala <87040483+itcelaya92@users.noreply.github.com>
2022-02-01 13:03:08 -0500

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const functionApiUrl = 'https://getresumecountercarlos.azurewebsites.net/api/GetResumeCounter?code=eL4tNslK3EW59ObcrAcvxzBUiuChSqVBCrH4PxLYemCIHo8c7VvwmQ==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
